9f701ddd28620a8d88a629c51553cbefdcd68e4a
Desray Mackay <desraymackay@gmail.com>
2024-05-31 23:12:39 +0200

    added search functionality to api

--- src/index.js
+++ src/index.js
@@ -2,41 +2,9 @@ function refreshWeather(response) {
 	let temperatureElement = document.querySelector("#temperature");
 	let temperature = response.data.temperature.current;
 	let cityElement = document.querySelector("#city");
-	let descriptionElement = document.querySelector("#description");
-	let humidityElement = document.querySelector("#humidity");
-	let windSpeedElement = document.querySelector("#wind-speed");
-	let timeElement = document.querySelector("#time");
-	let date = new Date(response.data.time * 1000);
-	let iconElement = document.querySelector("#icon");
 
 	cityElement.innerHTML = response.data.city;
-	timeElement.innerHTML = formatDate(date);
-	descriptionElement.innerHTML = response.data.condition.description;
-	humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-	windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 	temperatureElement.innerHTML = Math.round(temperature);
-	iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
-}
-
-function formatDate(date) {
-	let minutes = date.getMinutes();
-	let hours = date.getHours();
-	let days = [
-		"Sunday",
-		"Monday",
-		"Tuesday",
-		"Wednesday",
-		"Thursday",
-		"Friday",
-		"Saturday",
-	];
-	let day = days[date.getDay()];
-
-	if (minutes < 10) {
-		minutes = `0${minutes}`;
-	}
-
-	return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -48,11 +16,11 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
 	event.preventDefault();
 	let searchInput = document.querySelector("#search-form-input");
-	let cityElement = document.querySelector("#city");
-	cityElement.innerHTML = searchInput.value;
 
 	searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
