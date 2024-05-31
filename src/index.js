c7f2f7de5ddac0b2e39ade221b0d74b1fece4407
Desray Mackay <desraymackay@gmail.com>
2024-05-31 22:39:12 +0200

    added axios

--- src/index.js
+++ src/index.js
@@ -56,5 +56,3 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-searchCity("Paris");
