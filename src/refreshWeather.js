43c5bb9eac09c8cb157920ce6301ec88a4cd9e41
Desray Mackay <desraymackay@gmail.com>
2024-06-02 18:53:27 +0200

    linked my netlify account to Github

--- src/refreshWeather.js
+++ src/refreshWeather.js
@@ -15,9 +15,8 @@ function refreshWeather(response) {
 	humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 	windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 	temperatureElement.innerHTML = Math.round(temperature);
-	iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class="weather-app-icon"/>`;
+	iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class="weather-app-icon"> 
 }
-
 function formatDate(date) {
 	let minutes = date.getMinutes();
 	let hours = date.getHours();
@@ -33,25 +32,34 @@ function formatDate(date) {
 	let day = days[date.getDay()];
 
 	if (minutes < 10) {
-		minutes = `0${minutes} `;
+		minutes = `;
+	0;
+	$;
+	{
+		minutes;
+	}
+	`;
 	}
 
-	return `${day} ${hours}:${minutes}`;
+	return `;
+	$;
+	{
+		day;
+	}
+	$;
+	{
+		hours;
+	}
+	$;
+	{
+		minutes;
+	}
+	`;
 }
+
 function searchCity(city) {
 	let apiKey = "b2a5adcct04b33178913oc335f405433";
-	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&nits=metric`;
-	axios.get(apiUrl).then(refreshWeather);
+	let apiUrl = `;
+	//api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&nits=metric`;
+	https: axios.get(apiUrl).then(refreshWeather);
 }
-
-function handleSearchSubmit(event) {
-	event.preventDefault();
-	let searchInput = document.querySelector("#search-form-input");
-
-	searchCity(searchInput.value);
-}
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-searchCity("Paris");
